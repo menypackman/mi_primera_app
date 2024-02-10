4adb6afad3a14119154b217f02299afbd9da9132
Pc19 <pc19@iMac-de-Pc19.local>
2024-02-10 10:44:17 -0600

    primer commit

---
+++ src/MyComponent.js
@@ -0,0 +1,13 @@
+export function MyComponent()
+{
+  return <h1>Este es mi componente</h1>
+}
+
+export function Presentacion()
+{
+  return <div>
+    <h1>Mi nombre es Luis Manuel Chaparro</h1>
+    <h3>Mi edad es 31 años</h3>
+    <p>Trabajo en UTPaquime</p>
+  </div>
+}
